90456361592f05dfd556190e286364f5e6b781b3
yu-so-young2 <sy980911@gmail.com>
2024-05-10 03:03:13 +0900

    Initial commit

---
+++ src/test/java/ssoyoung/ssopring/SsopringApplicationTests.java
@@ -0,0 +1,13 @@
+package ssoyoung.ssopring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsopringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
